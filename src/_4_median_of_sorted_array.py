87013ec74aae2965f5c53b196b433743bfadd257
denis.bondarenko <denis.bondarenko@appfollow.io>
2022-04-28 13:41:58 +0400

    Fixed 4. median of two sorted arrays solution

--- src/_4_median_of_sorted_array.py
+++ src/_4_median_of_sorted_array.py
@@ -7,7 +7,7 @@ from typing import List
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         if nums1 and nums2:
-            nums = tuple(chain(nums1, nums2))
+            nums = sorted(chain(nums1, nums2))
         elif nums1:
             nums = nums1
         else:
