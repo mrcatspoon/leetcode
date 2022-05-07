84d0a88534f75ac11b422f276482d9f54e2c42ba
denis.bondarenko <denis.bondarenko@appfollow.io>
2022-05-07 19:34:52 +0400

    Add 6 solution

---
+++ src/_6_zigzag.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/zigzag-conversion/
+from collections import defaultdict
+
+
+class Solution:
+    __slots__ = ()
+
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+
+        num_rows_range = tuple(range(numRows))
+        zig_range = num_rows_range[-2:0:-1]
+
+        s_iter = iter(s)
+        zig_rows = defaultdict(list)
+        try:
+            while True:
+                for i in num_rows_range:
+                    zig_rows[i].append(next(s_iter))
+                for i in zig_range:
+                    zig_rows[i].append(next(s_iter))
+        except StopIteration:
+            pass
+
+        return "".join(char for i in num_rows_range for char in zig_rows[i])
