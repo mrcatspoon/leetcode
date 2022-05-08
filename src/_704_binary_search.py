d7e76447ec52cb2d4de0ce53225b517c4e469776
denis.bondarenko <denis.bondarenko@appfollow.io>
2022-05-08 09:25:36 +0400

    Add 704 solution

---
+++ src/_704_binary_search.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/binary-search/
+from typing import List
+
+
+class Solution:
+    __slots__ = ()
+
+    def search(self, nums: List[int], target: int) -> int:
+        low, high = 0, len(nums)
+        while low < high:
+            mid = (low + high) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid
+        return -1
