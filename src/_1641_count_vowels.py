79a87798e5a5030265d2eed15bcdec19b00e29e0
denis.bondarenko <denis.bondarenko@appfollow.io>
2022-05-11 14:11:35 +0400

    Add 1641 solution

---
+++ src/_1641_count_vowels.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/count-sorted-vowel-strings/
+
+
+class Solution:
+    def countVowelStrings(self, n: int) -> int:
+        return int((n + 4) * (n + 3) * (n + 2) * (n + 1) / 24)
