59c0e0ae12e57d3f21d68a81845eea2fcea6d206
denis.bondarenko <denis.bondarenko@appfollow.io>
2022-05-17 11:58:35 +0400

    Add 144 solution

---
+++ src/_144_binary_tree_traversal.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/binary-tree-preorder-traversal/
+from typing import List, Optional
+
+
+class TreeNode:
+    __slots__ = ("val", "left", "right")
+
+    def __init__(self, val: int = 0, left: Optional["TreeNode"] = None, right: Optional["TreeNode"] = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __repr__(self) -> str:
+        return str(self.val)
+
+
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+
+        result = []
+        self.traverse(root, result)
+        return result
+
+    def traverse(self, root: Optional[TreeNode], result: List[int]):
+        result.append(root.val)
+        if root.left:
+            self.traverse(root.left, result)
+        if root.right:
+            self.traverse(root.right, result)
