dd4e2c728060941dd05e1dcc4d8a153c01dfc854
denis.bondarenko <denis.bondarenko@appfollow.io>
2022-05-18 09:22:47 +0400

    Add 46 solution

---
+++ src/_46_permutations.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/permutations/
+from itertools import permutations
+from typing import List
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        return list(map(list, permutations(nums, len(nums))))
